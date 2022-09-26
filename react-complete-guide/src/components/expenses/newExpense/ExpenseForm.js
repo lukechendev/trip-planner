137ecf18ad06907fd86583d3d4dc95512877820d
Luke Chen <lchen.dev@gmail.com>
2022-09-25 23:06:47 -0400

    double binding

--- react-complete-guide/src/components/expenses/newExpense/ExpenseForm.js
+++ react-complete-guide/src/components/expenses/newExpense/ExpenseForm.js
@@ -6,35 +6,86 @@ const ExpenseForm = () => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
+
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // }); // Not good as userInput could be out of date
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // }); // Safe, prevState is always up to date
+
         console.log("title changed " + event.target.value);
-    }
+    };
 
     const amountChangeHanddler = (event) => {
         setEnteredAmount(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // }); // Not good as userInput could be out of date
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // }); // Safe, prevState is always up to date
+
         console.log("amount changed " + event.target.value);
-    }
+    };
 
     const dateChangeHanddler = (event) => {
         setEnteredDate(event.target.value);
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // }); // Not good as userInput could be out of date
+
+        // setUserInput((prevState) => {
+        //     return { ...prevState, enteredDate: event.target.value };
+        // }); // Safe, prevState is always up to date
+
         console.log("date changed " + event.target.value);
-    }
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHanddler} />
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHanddler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHanddler} />
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHanddler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
