ad2982c216fe073869ef66107e608f2c7510b90b
Luke Chen <lchen.dev@gmail.com>
2022-09-25 23:40:09 -0400

    lift data to App.js

--- react-complete-guide/src/components/expenses/newExpense/ExpenseForm.js
+++ react-complete-guide/src/components/expenses/newExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
@@ -67,6 +67,8 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
+
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
