99cffb3d656e6505ced7e3be5af4cebac67eaa26
Luke Chen <lchen.dev@gmail.com>
2022-09-27 21:33:09 -0400

    cleanup

--- react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -1,32 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../ui/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const dateFormat = {
-    month: 'long', day: '2-digit', year: 'numeric',
-    hours: '2-digit', minute: '2-digit', second: '2-digit'
-};
-
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
     console.log('ExpenseItem evaluated by React');
-
-    const clickHandler = () => {
-        console.log('Title update triggered!');
-
-        const currentTime = new Date(Date.now()).toLocaleString('en-US', dateFormat);
-        setTitle('New Title@' + currentTime);
-    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
