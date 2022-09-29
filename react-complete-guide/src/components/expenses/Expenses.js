ca9ee6710fdb54760306eb945b3e9d103f662152
Luke Chen <lchen.dev@gmail.com>
2022-09-29 08:54:14 -0400

    Add empty list message

--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -15,16 +15,21 @@ const Expenses = (props) => {
     const filteredExpenses =
         props.expenses.filter(expense => expense.date.getFullYear().toString() === yearToFilter);
 
+    let expensesContent = <p>No expenses found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} />
+        ));
+    };
+
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={yearToFilter} onSelected={filteringHandler} />
-            {filteredExpenses.map(expense => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date} />
-            ))}
+            {expensesContent}
         </Card>
     );
 }
